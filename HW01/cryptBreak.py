a906e2b79a6a4d18a4434edf06e0048001ab670a
AnishLaddha <anishladdha03@gmail.com>
2024-01-13 22:24:00 -0500

    first commit

---
+++ HW01/cryptBreak.py
@@ -0,0 +1,17 @@
+from BitVector import *
+def cryptBreak(cyphertextFile, key_bv):
+    
+    return
+
+# PassPhrase = "Hopes and dreams of a million years"                            #(C)
+
+# BLOCKSIZE = 64                                                                #(D)
+# numbytes = BLOCKSIZE // 8  
+
+# bv_iv = BitVector(bitlist = [0]*BLOCKSIZE)    
+# for i in range(0,len(PassPhrase) // numbytes):                                #(G)
+#     textstr = PassPhrase[i*numbytes:(i+1)*numbytes]
+#     bv_iv ^= BitVector( textstring = textstr )  
+# print(bv_iv.length())
+# print(len(PassPhrase))
+
